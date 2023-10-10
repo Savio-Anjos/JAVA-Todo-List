627dd2f9d7639ee6a0da0b4d1e62efca90b18f5b
Savio-Anjos <savioanjos2020@gmail.com>
2023-10-10 14:08:06 -0300

    :sparkles: Configuring Lombok

--- src/main/java/br/com/savioanjos/todolist/user/UserModel.java
+++ src/main/java/br/com/savioanjos/todolist/user/UserModel.java
@@ -1,33 +1,12 @@
 package br.com.savioanjos.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     private String username;
     private String name;
     private String password;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
