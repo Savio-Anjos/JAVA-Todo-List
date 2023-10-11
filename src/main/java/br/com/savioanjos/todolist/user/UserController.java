ef19c9b0ccfb3ea5739dd7eb72a05a9377b1d492
Savio-Anjos <savioanjos2020@gmail.com>
2023-10-10 23:18:31 -0300

    :sparkles: Configuring returns

--- src/main/java/br/com/savioanjos/todolist/user/UserController.java
+++ src/main/java/br/com/savioanjos/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.savioanjos.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +16,16 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            System.out.println("Usuario Já existe");
-            return null;
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
