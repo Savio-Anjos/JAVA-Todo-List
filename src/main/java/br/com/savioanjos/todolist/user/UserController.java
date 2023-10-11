9993f03eea40e97d0918ab217254739310058294
Savio-Anjos <savioanjos2020@gmail.com>
2023-10-10 21:07:58 -0300

    :sparkles: Create repository

--- src/main/java/br/com/savioanjos/todolist/user/UserController.java
+++ src/main/java/br/com/savioanjos/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.savioanjos.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.print(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 
 }
